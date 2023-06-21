ad12983bd93296792abf4fb738360c2eb1d78ef6
safirasn21 <safirasn021@gmai.com>
2023-06-21 11:11:32 +0700

    loop untuk menulis dan loop akan berhenti jika memasukkan karakter q

--- IoFileWritePath/IoFileWritePath.cpp
+++ IoFileWritePath/IoFileWritePath.cpp
@@ -16,5 +16,15 @@ int main() {
 	outfile.open(namafile + ".txt", ios::out);
 
 	cout << ">= menulis file, \'q\' untuk keluar" << endl;
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari baris kedalam file
+		outfile << baris << endl;
+	}
 
 }
