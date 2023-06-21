4f160efcfebdec4c0775efd3fb0fe2c9e8f2a8dd
safirasn21 <safirasn021@gmai.com>
2023-06-21 11:02:44 +0700

    jika terdapat file maka melakukan perulangan setiap baris dan melakukan penutupan file

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -33,5 +33,15 @@ int main() {
 		infile.open("contohfile.txt");
 
 		cout << endl << ">= Membuka dan membaca file " << endl;
+		//jika file ada maka
+		if (infile.is_open()) {
+			//melakukan perulangan setiap baris
+			while (getline(infile, baris)) {
+				//dan tampilan di sini
+				cout << baris << '\n';
+			}
+			//tutup file tersebut setelah selesai
+			infile.close();
+		}
 		
 }
