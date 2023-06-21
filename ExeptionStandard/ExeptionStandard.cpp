d2b4ecd32315e7df7dc8c8295be752f7f0335804
safirasn21 <safirasn021@gmai.com>
2023-06-21 11:33:09 +0700

    menambahkan flag catch

--- ExeptionStandard/ExeptionStandard.cpp
+++ ExeptionStandard/ExeptionStandard.cpp
@@ -13,5 +13,9 @@ int main() {
 		cout << data.at(5) << endl;
 		//memanggil array elemen ke 5
 	}
-
+	catch (exception& e) {
+		//penangkap menggunakan object exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memilki 3 elemen*/
+	}
 }
