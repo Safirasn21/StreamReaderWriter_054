f5e41965d2f0b0c1dd587a37db889aba2df6bcc4
safirasn21 <safirasn021@gmai.com>
2023-06-21 11:09:44 +0700

    menambahkan file baru

--- IoFileWritePath/IoFileWritePath.cpp
+++ IoFileWritePath/IoFileWritePath.cpp
@@ -10,18 +10,4 @@ int main() {
 	cout << "Masukan nama file : ";
 	cin >> namafile;
 
-	//membuka file dalam menulis
-	ofstream outfile;
-	//menujuk kesebuah nama file
-	outfile.open(namafile + ".txt", ios::out);
-
-	cout << ">= menulis file, \'q\' untuk keluar" << endl;
-
-	//unlimited loop untuk menulis
-	while (true) {
-		cout << "- ";
-		//mendapatkan setiap karakter dalam satu baris
-		getline(cin, baris);
-		//loop akan berhenti jika anda memasukkan karakter q
-	}
 }
